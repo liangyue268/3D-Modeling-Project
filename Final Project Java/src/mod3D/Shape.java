672c36acbd77d715e64a2cb17ce09b1725065d47
pvishwak <pvishwak@stevens.edu>
2015-12-01 16:13:26 -0500

    Edition in abstract class

--- Final Project Java/src/mod3D/Shape.java
+++ Final Project Java/src/mod3D/Shape.java
@@ -1,13 +1,31 @@
 package mod3D;
 
+import java.awt.Graphics;
+
+
+
 public abstract class Shape {
-	private int x, y, z, color;
+	private int x, y, z, color, theta;
 	
 	private void print(){}
 	
 	public void getcolor(){}
 	
 	public void setcolor(){}
+	
+	public abstract void translate(int x, int y, int z);
+	
+	public abstract void rotate(int theta, int x, int y, int z);
+	
+	public abstract void scale(int x, int y, int z);
+	
+	
+	public abstract void union(int x, int y, int z);
+	
+	public abstract void substract(int x, int y, int z);
+	
+	public abstract void intersect(int x, int y, int z);
+	
 	
 
 }
