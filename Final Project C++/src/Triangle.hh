479f4b7a11adb7bc158238b8e2183a5d82956424
Carly Bean <cbean@stevens.edu>
2015-12-01 22:39:22 -0500

    included Shape2D

--- Final Project C++/src/Triangle.hh
+++ Final Project C++/src/Triangle.hh
@@ -13,7 +13,7 @@ private:
 	int x2, y2;
 	int x3 ,y3;
 public:
-	Triangle(int x1, int y1 int x2, int y2 int x3, int y3 int x, int y, Color c) : Shape2D(x,y), x1(x1), y1(y1), x2(x2), y2(y2), x3(x3), y3(y3) {}
+	Triangle(int x1, int y1, int x2, int y2, int x3, int y3, int x, int y, Color c) : Shape2D(x,y), x1(x1), y1(y1), x2(x2), y2(y2), x3(x3), y3(y3) {}
 
 	double getArea(){ //A = abs[(x1(y2-y3) + x2(y3-y1) + x3(y1-y2))/2]
 		return abs((x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2))/2);
