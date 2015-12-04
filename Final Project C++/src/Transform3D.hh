e19f5958027ab96fae0ec0d868bff99639f8a8f7
cscoyle <cscoyle4@gmail.com>
2015-12-04 11:50:31 -0500

    Added Transformation3D comments

--- Final Project C++/src/Transform3D.hh
+++ Final Project C++/src/Transform3D.hh
@@ -1,9 +1,4 @@
-/*
- * Transform3D.hh
- *
- *  Created on: Dec 2, 2015
- *      Author: Class2015
- */
+//Authors: Carly Bean, Chris Coyle, Yue Liang
 
 #ifndef TRANSFORM3D_HH_
 #define TRANSFORM3D_HH_
@@ -16,6 +11,9 @@ class Transform3D {
 private:
 	Shape3D* d;
 	int n;
+
+//a Transform3D object is passed an array of Shape3D objects, upon which it will apply designated transformations
+//the purpose for this is to be able to apply designated changes to multiple Shape3D objects at once.
 public:
 	Transform3D(Shape3D d[], int n) : n(n) {
 		this->d = new Shape3D*[n];
@@ -23,6 +21,9 @@ public:
 			this->d[i] = &d[i];
 		}
 	}
+
+//Transformations are made by calling the methods translate, changeOrientation and	scale.
+//Transformation3D's methods call methods within Shape3D, which actually apply the change.
 
 	void translate(Point trans){
 		for(int i = 0; i < n; i++) {
@@ -37,7 +38,7 @@ public:
 		}
 	}
 
-	void scale(double s) {  //Pass the method a Sphere and a scale double
+	void scale(double s) {
 		for(int i = 0; i < n; i++) {
 			d[i].scale(s);
 		}
