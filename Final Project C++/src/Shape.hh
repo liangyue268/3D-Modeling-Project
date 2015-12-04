c8e3af4ff854559ebaace25cfcb69b48cdf565bf
Carly Bean <cbean@stevens.edu>
2015-12-04 08:35:04 -0500

    Added comments

--- Final Project C++/src/Shape.hh
+++ Final Project C++/src/Shape.hh
@@ -1,3 +1,4 @@
+//Authors: Carly Bean, Chris Coyle, Yue Liang
 #ifndef SHAPE_HH_
 #define SHAPE_HH_
 
@@ -7,22 +8,26 @@ using namespace std;
 
 const double PI = 3.1415926535898;
 typedef int Color;
+
+//Abstract class defining a shape's color and center point
 class Shape {
+//Made data public bc the point is to be able to manipulate these shapes
 public:
 	Color color;
 	Point center;
 
-	//print the shape
-	virtual void print(const char* filename) {
-		cout << "print shape." << '\n';
-	}
+	//pure virtual function to print a shape to STL format
+	virtual void print(const char* filename) = 0;
 
+	//Constructor
 	Shape(Point p,  Color c) : center(p), color(c) { }
 
-	Color getColor() {
+	//Method to retrieve a shape's color ---shouldn't need any more if color is public
+	Color getColor() const {
 		return color;
 	}
 
+	//Method to change a shape's color ---shouldn't need any more if color is public
 	void setColor(Color c) {
 		color = c;
 	}
