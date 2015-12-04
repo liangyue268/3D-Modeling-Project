0479ebee45aa78aa4ec184d328912c55fcd58007
RayaRahman <rrahman1@stevens.edu>
2015-12-04 12:57:00 -0500

    "Added Main Class"

--- Final Project Java/src/mod3D/MainClass.java
+++ Final Project Java/src/mod3D/MainClass.java
@@ -2,10 +2,10 @@ package mod3D;
 
 public class MainClass {
 
-	// Execution will start from here
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Main Class");
 	}
 
 }
