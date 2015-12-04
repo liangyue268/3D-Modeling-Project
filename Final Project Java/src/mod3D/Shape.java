3bdcd8d5235c01c8ff3f2a74cc499770ca203d38
cscoyle <cscoyle4@gmail.com>
2015-12-03 23:44:37 -0500

    .

--- Final Project Java/src/mod3D/Shape.java
+++ Final Project Java/src/mod3D/Shape.java
@@ -15,7 +15,7 @@ public abstract class Shape {
 	Shape( int center[], Color c){
 		this.center[0] = center[0];
 		this.center[1] = center[1];
-		this.center[1] = center[1];
+		this.center[2] = center[2];
 		
 	}
 	
