c509c8b53613a0722736c7f11041b3679ef77e49
cscoyle <cscoyle4@gmail.com>
2015-12-04 11:59:46 -0500

    Comments for SHape2D

--- Final Project C++/src/Shape2D.hh
+++ Final Project C++/src/Shape2D.hh
@@ -4,6 +4,8 @@
 #include "Shape.hh"
 class Shape2D : public Shape {
 public:
+//Shape2D class represents a 2D Shape with an x & y center point with a 0 z-value, and a color;
+//SHape2D is a child of Shape.
 	Shape2D(int x, int y, Color c) : Shape(Point(x, y, 0), c) { }
 
 //(x & y are the coordinates of the center point)
@@ -15,6 +17,8 @@ public:
 	//virtual Shape2D substract(Shape2D that) = 0;
 
 	//@return the area of the Shape
+
+//Each child of Shape2D should be able to determine its own area.
 	virtual double getArea() = 0;
 };
 
