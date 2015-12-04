f8b5d57e835b90ff9792912a908afda835f6ef8f
Carly Bean <cbean@stevens.edu>
2015-12-04 09:17:07 -0500

    File for class render

---
+++ Final Project C++/src/Render.hh
@@ -0,0 +1,29 @@
+#include "Facet.hh"
+#include <iostream>
+using namespace std;
+
+class Render {
+private:
+public:
+	//Constructor
+	Render(){
+
+	}
+
+	//Returns pointer to array of facets (traingles) that make up sphere
+	int*  rendSphere(){
+
+	}
+
+	//Returns pointer to array of facets (traingles) that make up cube
+	int* rendCube(){
+
+	}
+
+	//Returns pointer to array of facets (traingles) that make up cylinder
+	int* rendCylinder(){
+
+	}
+
+
+};
