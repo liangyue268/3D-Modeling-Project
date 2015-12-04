4d88f5798cf48d59c492f46a946337815445e5c7
cscoyle <cscoyle4@gmail.com>
2015-12-03 22:59:04 -0500

    Worked on Sphere Class

--- Final Project Java/src/mod3D/Sphere.java
+++ Final Project Java/src/mod3D/Sphere.java
@@ -1,10 +1,19 @@
 package mod3D;
+import java.awt.Color;
 
-public class Sphere {
+public class Sphere extends Shape3D {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//test
+public double radius;
+
+public Sphere(double r, double orientation[], Color c, int center[]){
+	super(center, c, orientation);
+	radius = r;
+	
+	
+	
+	
+}
+	
+	
 	}
 
-}
