11c5513ff522b22ee4e4a443349e99eb18add94f
cscoyle <cscoyle4@gmail.com>
2015-12-03 22:18:56 -0500

    Created class Point, and worked on class Shape, and class Shape3D

--- Final Project Java/src/mod3D/Shape.java
+++ Final Project Java/src/mod3D/Shape.java
@@ -1,31 +1,20 @@
 package mod3D;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 
 
 public abstract class Shape {
-	private int x, y, z, color, theta;
 	
-	private void print(){}
+	public Color c;
 	
-	public void getcolor(){}
+	public Point center;
 	
-	public void setcolor(){}
-	
-	public abstract void translate(int x, int y, int z);
-	
-	public abstract void rotate(int theta, int x, int y, int z);
-	
-	public abstract void scale(int x, int y, int z);
-	
-	
-	public abstract void union(int x, int y, int z);
-	
-	public abstract void substract(int x, int y, int z);
-	
-	public abstract void intersect(int x, int y, int z);
-	
+	Shape( Point p, Color c){
+		center = p;
+		
+	}
 	
 
 }
