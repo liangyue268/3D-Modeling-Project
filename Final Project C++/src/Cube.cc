8f81a726ee4613f157edb5df31140a0a90e1ee45
Carly Bean <cbean@stevens.edu>
2015-12-16 14:17:30 -0500

    added render function

--- Final Project C++/src/Cube.cc
+++ Final Project C++/src/Cube.cc
@@ -1,5 +1,6 @@
 //Authors: Carly Bean, Chris Coyle, Yue Linag
 #include "Cube.hh"
+#include <fstream>
 
 bool const Cube::isInside(Point p) {
 	if(abs(p.x() - center.x()) <= (length / 2) && abs(p.y() - center.y()) <= (width / 2) && abs(p.z() - center.z()) <= (height / 2)) {
@@ -22,4 +23,104 @@ void Cube::scale(double s){
 	length = s*length;
 	width = s*width;
 	height = s*height;
+}
+
+//creates file "Cube.stl"
+void Cube::render(){
+		ofstream file;
+		file.open("Cube.stl");
+		file << "solid OpenSCAD_Model\n";
+		//face 1
+		file << "  facet normal 0 0 1\n";
+		file << "    outer loop\n";
+		file << "      vertex 0 " << width << ' ' << height << '\n';
+		file << "      vertex " << length << " 0 " << height << '\n';
+		file << "      vertex " << length << ' ' << width << ' ' << height << '\n';
+		file << "    endloop\n";
+		file << "  endfacet\n";
+		file << "  facet normal 0 0 1\n";
+		file << "    outer loop\n";
+		file << "      vertex " << length << ' ' << '0' << ' ' << height << '\n';
+		file << "      vertex " << '0' << ' ' << width << ' ' << height << '\n';
+		file << "      vertex " << '0' << ' ' << '0' << ' ' << height << '\n';
+		file << "    endloop\n";
+		file << "  endfacet\n";
+		//face 2
+		file << "  facet normal 0 0 -1\n";
+		file << "    outer loop\n";
+		file << "      vertex " << '0' << ' ' << '0' << ' ' << '0' << '\n';
+		file << "      vertex " << length << ' ' << width << ' ' << '0' << '\n';
+		file << "      vertex " << length << ' ' << '0' << ' ' << '0' << '\n';
+		file << "    endloop\n";
+		file << "  endfacet\n";
+		file << "  facet normal 0 0 -1\n";
+		file << "    outer loop\n";
+		file << "      vertex " << length << ' ' << width << ' ' << '0' << '\n';
+		file << "      vertex " << '0' << ' ' << '0' << ' ' << '0' << '\n';
+		file << "      vertex " << '0' << ' ' << width << ' ' << '0' << '\n';
+		file << "    endloop\n";
+		file << "  endfacet\n";
+		//face 3
+		file << "  facet normal 0 -1 0\n";
+		file << "    outer loop\n";
+		file << "      vertex " << '0' << ' ' << '0' << ' ' << '0' << '\n';
+		file << "      vertex " << length << ' ' << '0' << ' ' << height << '\n';
+		file << "      vertex " << '0' << ' ' << '0' << ' ' << height << '\n';
+		file << "    endloop\n";
+		file << "  endfacet\n";
+		file << "  facet normal 0 -1 -0\n";
+		file << "    outer loop\n";
+		file << "      vertex " << length << ' ' << '0' << ' ' << height << '\n';
+		file << "      vertex " << '0' << ' ' << '0' << ' ' << '0' << '\n';
+		file << "      vertex " << length << ' ' << '0' << ' ' << '0' << '\n';
+		file << "    endloop\n";
+		file << "  endfacet\n";
+		//face 4
+		file << "  facet normal -1 0 0\n";
+		file << "    outer loop\n";
+		file << "      vertex " << '0' << ' ' << '0' << ' ' << '0' << '\n';
+		file << "      vertex " << '0' << ' ' << width << ' ' << height << '\n';
+		file << "      vertex " << '0' << ' ' << width << ' ' << '0' << '\n';
+		file << "    endloop\n";
+		file << "  endfacet\n";
+		file << "  facet normal -1 0 0\n";
+		file << "    outer loop\n";
+		file << "      vertex " << '0' << ' ' << '0' << ' ' << '0' << '\n';
+		file << "      vertex " << '0' << ' ' << width << ' ' << height << '\n';
+		file << "      vertex " << '0' << ' ' << '0' << ' ' << height << '\n';
+		file << "    endloop\n";
+		file << "  endfacet\n";
+		//face 5
+		file << "  facet normal 0 1 0\n";
+		file << "    outer loop\n";
+		file << "      vertex " << '0' << ' ' << width << ' ' << '0' << '\n';
+		file << "      vertex " << length << ' ' << width << ' ' << height << '\n';
+		file << "      vertex " << '0' << ' ' << width << ' ' << height << '\n';
+		file << "    endloop\n";
+		file << "  endfacet\n";
+		file << "  facet normal 0 1 0\n";
+		file << "    outer loop\n";
+		file << "      vertex " << '0' << ' ' << width << ' ' << '0' << '\n';
+		file << "      vertex " << length << ' ' << width << ' ' << height << '\n';
+		file << "      vertex " << length << ' ' << width << ' ' << '0' << '\n';
+		file << "    endloop\n";
+		file << "  endfacet\n";
+		//face 6
+		file << "  facet normal 1 0 0\n";
+		file << "    outer loop\n";
+		file << "      vertex " << length << ' ' << '0' << ' ' << '0' << '\n';
+		file << "      vertex " << length << ' ' << width << ' ' << height << '\n';
+		file << "      vertex " << length << ' ' << width << ' ' << '0' << '\n';
+		file << "    endloop\n";
+		file << "  endfacet\n";
+		file << "  facet normal 1 0 0\n";
+		file << "    outer loop\n";
+		file << "      vertex " << length << ' ' << '0' << ' ' << '0' << '\n';
+		file << "      vertex " << length << ' ' << width << ' ' << height << '\n';
+		file << "      vertex " << length << ' ' << '0' << ' ' << height << '\n';
+		file << "    endloop\n";
+		file << "  endfacet\n";
+
+		file << "endsolid OpenSCAD_Model\n";
+		file.close();
 }
--- Final Project C++/src/Cube.hh
+++ Final Project C++/src/Cube.hh
@@ -3,7 +3,6 @@
 
 #include "Shape3D.hh"
 #include <iostream>
-#include <fstream>
 #include <cmath>
 using namespace std;
 
@@ -22,61 +21,7 @@ public:
 
 	double const getArea();
 	void scale(double s);
-/*
-	void print(const char* filename) {
-		//cout << "print cube." << '\n';
-		//file << "      vertex " << length << ' ' << width << ' ' << height << '\n';
-		ofstream file(filename);
-		file << "solid OpenSCAD_Model\n";
-		//face 1
-		file << "  facet normal -0 0 1\n";
-		file << "    outer loop\n";
-		file << "      vertex 0 " << width << ' ' << height << '\n';
-		file << "      vertex " << length << " 0 " << height << '\n';
-		file << "      vertex " << length << ' ' << width << ' ' << height << '\n';
-		file << "    endloop\n";
-		file << "  endfacet\n";
-		file << "  facet normal 0 0 1\n";
-		file << "    outer loop\n";
-		file << "      vertex " << length << ' ' << '0' << ' ' << height << '\n';
-		file << "      vertex " << '0' << ' ' << width << ' ' << height << '\n';
-		file << "      vertex " << '0' << ' ' << '0' << ' ' << height << '\n';
-		file << "    endloop\n";
-		file << "  endfacet\n";
-		//face 2
-		file << "  facet normal 0 0 -1\n";
-		file << "    outer loop\n";
-		file << "      vertex " << '0' << ' ' << '0' << ' ' << '0' << '\n';
-		file << "      vertex " << length << ' ' << width << ' ' << '0' << '\n';
-		file << "      vertex " << length << ' ' << '0' << ' ' << '0' << '\n';
-		file << "    endloop\n";
-		file << "  endfacet\n";
-		file << "  facet normal -0 0 -1\n";
-		file << "    outer loop\n";
-		file << "      vertex " << length << ' ' << width << ' ' << '0' << '\n';
-		file << "      vertex " << '0' << ' ' << '0' << ' ' << '0' << '\n';
-		file << "      vertex " << '0' << ' ' << width << ' ' << '0' << '\n';
-		file << "    endloop\n";
-		file << "  endfacet\n";
-		//face 3
-		file << "  facet normal 0 -1 0\n";
-		file << "    outer loop\n";
-		file << "      vertex " << '0' << ' ' << '0' << ' ' << '0' << '\n';
-		file << "      vertex " << length << ' ' << '0' << ' ' << height << '\n';
-		file << "      vertex " << '0' << ' ' << '0' << ' ' << height << '\n';
-		file << "    endloop\n";
-		file << "  endfacet\n";
-		file << "  facet normal 0 -1 -0\n";
-		file << "    outer loop\n";
-		file << "      vertex " << length << ' ' << '0' << ' ' << height << '\n';
-		file << "      vertex " << '0' << ' ' << '0' << ' ' << '0' << '\n';
-		file << "      vertex " << length << ' ' << '0' << ' ' << '0' << '\n';
-		file << "    endloop\n";
-		file << "  endfacet\n";
-
-		file << "endsolid OpenSCAD_Model\n";
-	}
-*/
+	void render();
 	friend ostream& operator <<(ostream& s, const Cube& c) {
 		s << "cube(" << c.length << ", " << c.width << ", " << c.height << ")";
 		return s;
