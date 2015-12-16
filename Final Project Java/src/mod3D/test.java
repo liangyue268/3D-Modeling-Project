187e1347704def4ea2941c63cce1225b81e22c0b
RayaRahman <rrahman1@stevens.edu>
2015-12-16 01:59:08 -0500

    Modified Render class and Test class to display a Icosphere.

--- Final Project Java/src/mod3D/test.java
+++ Final Project Java/src/mod3D/test.java
@@ -20,38 +20,31 @@ public class test extends PApplet{
 
 	int moveX = 0;
 
+	int sphereCenter[] = {0,0,0};
+	float sphereVerts[][] = Render.Rend(new Sphere(150, null, null, sphereCenter));
 	public void draw() {
 
 		float rx = frameCount / 800.f;
 		float ry = frameCount / 430.f;
+		
+		
 		translate(width / 2, height / 2);
 		background(0);
 		noFill();
-		PVector f1 = new PVector();
-		PVector f2 = new PVector();
-		PVector f3 = new PVector();
 		stroke(255);
 		strokeWeight(2);
 		pushMatrix();
 		rotateX(rx);
 		rotateY(ry);
-		for (int i = 0; i < ico.vertexList.size(); i += 9) {
-			f1.x = ico.vertexList.get(i) * 100;
-			f1.y = ico.vertexList.get(i + 1) * 100;
-			f1.z = ico.vertexList.get(i + 2) * 100;
-			f2.x = ico.vertexList.get(i + 3) * 100;
-			f2.y = ico.vertexList.get(i + 4) * 100;
-			f2.z = ico.vertexList.get(i + 5) * 100;
-			f3.x = ico.vertexList.get(i + 6) * 100;
-			f3.y = ico.vertexList.get(i + 7) * 100;
-			f3.z = ico.vertexList.get(i + 8) * 100;
+		for(int i=0; i < sphereVerts.length; i++){
 			beginShape();
-			vertex(f1.x, f1.y, f1.z);
-			vertex(f2.x, f2.y, f2.z);
-			vertex(f3.x, f3.y, f3.z);
+			vertex(sphereVerts[i][0], sphereVerts[i][1], sphereVerts[i][2]);
+			vertex(sphereVerts[i][3], sphereVerts[i][4], sphereVerts[i][5]);
+			vertex(sphereVerts[i][6], sphereVerts[i][7], sphereVerts[i][8]);
 			endShape(CLOSE);
-
 		}
+		
+		
 		popMatrix();
 
 	}
