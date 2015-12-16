4b55ec9c1472baedac44c98c3bfa12dbe3dc8c32
pvishwak <pvishwak@stevens.edu>
2015-12-16 15:42:00 -0500

    sphere for master

--- Final Project Java/src/mod3D/Render.java
+++ Final Project Java/src/mod3D/Render.java
@@ -1,9 +1,9 @@
 package mod3D;
-import processing.core.PApplet;
 import java.applet.Applet;
 import java.awt.BorderLayout;
 import java.awt.GraphicsConfiguration;
 import java.util.Enumeration;
+
 import javax.media.j3d.Alpha;
 import javax.media.j3d.Appearance;
 import javax.media.j3d.Background;
@@ -18,35 +18,77 @@ import javax.media.j3d.Shape3D;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
 import javax.media.j3d.WakeupOnElapsedFrames;
+import processing.core.PVector;
 
+import javax.vecmath.Color3f;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
 
+import com.sun.j3d.utils.applet.MainFrame;
+import com.sun.j3d.utils.universe.SimpleUniverse;
 
 
 public class Render {
 
-	
-		Render(Shape3D[] d, int numShapes){
+	Render(Shape3D[] d, int numShapes) {
+
+	}
+
+	int[] Rend(Cube c) {
+		Icocube ico = new Icocube();
+		PVector f1 = new PVector();
+		PVector f2 = new PVector();
+		PVector f3 = new PVector();
+		int e[] = {};
+
+		return e;
+	}
+
+	int[] Rend(Cylinder c) {
+
+		int e[] = {};
+
+		return e;
+	}
+
+	static float[][] Rend(Sphere s) {
+		Icosphere ico = new Icosphere();
+		PVector f1 = new PVector();
+		PVector f2 = new PVector();
+		PVector f3 = new PVector();
+		int counter = 0;
+		float e[][] = new float[ico.vertexList.size()/9][9];
+		
+		for (int i = 0; i < ico.vertexList.size(); i += 9) {
 			
+			f1.x = (float) (ico.vertexList.get(i) * s.radius);
+			f1.y = (float) (ico.vertexList.get(i + 1) * s.radius);
+			f1.z = (float) (ico.vertexList.get(i + 2) * s.radius);
+			f2.x = (float) (ico.vertexList.get(i + 3) * s.radius);
+			f2.y = (float) (ico.vertexList.get(i + 4) * s.radius);
+			f2.z = (float) (ico.vertexList.get(i + 5) * s.radius);
+			f3.x = (float) (ico.vertexList.get(i + 6) * s.radius);
+			f3.y = (float) (ico.vertexList.get(i + 7) * s.radius);
+			f3.z = (float) (ico.vertexList.get(i + 8) * s.radius);
 			
+			e[counter][0] = f1.x;
+			e[counter][1] = f1.y;
+			e[counter][2] = f1.z;
+			
+			e[counter][3] = f2.x;
+			e[counter][4] = f2.y;
+			e[counter][5] = f2.z;
+			
+			e[counter][6] = f3.x;
+			e[counter][7] = f3.y;
+			e[counter][8] = f3.z;
+			
+			counter++;
+
 		}
-	
 		
-	int[] Rend(Cube c){
-		
-		int e[] = {};
-	
-		return e; }	
-	
-	int[] Rend(Cylinder c){
-		
-		int e[] = {};
-	
-		return e; }
-	
-	int[] Rend(Sphere s){
-		
-		int e[] = {};
-	
-		return e; }	
+
+		return e;
+	}
 
 }
