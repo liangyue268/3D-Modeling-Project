17aaf9963c31d426ebe96fcc0de41b2ce831226e
cscoyle <cscoyle4@gmail.com>
2015-12-17 15:41:54 -0500

    added comments

--- Final Project Java/src/mod3D/Shape.java
+++ Final Project Java/src/mod3D/Shape.java
@@ -1,11 +1,16 @@
+//Author: Raya Rahman
+
 package mod3D;
 
 import java.awt.Color;
 import java.awt.Graphics;
 
 
+//Abstract class defining a shape, by it's color and center point
 
 public abstract class Shape {
+
+	//Made data public bc the point is to be able to manipulate these shapes	
 	public int center[] = new int[3];
 	
 	public Color c;
