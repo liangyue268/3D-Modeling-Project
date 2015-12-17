bb07da1e57d5d5415f6fb0bd2b228ba1b4f0233c
cscoyle <cscoyle4@gmail.com>
2015-12-17 15:42:59 -0500

    Added Comments

--- Final Project Java/src/mod3D/Shape3D.java
+++ Final Project Java/src/mod3D/Shape3D.java
@@ -1,7 +1,11 @@
+//Author: Christopher Coyle
+
 package mod3D;
 
 import java.awt.Color;
 
+//class Shape3D represents all 3D shapes; it is a child of class Shape
+//A Shape3D is defined by all parameters of a shape with the addition of an orientation vector, which determines what direction the 3DShape is pointing
 public abstract class Shape3D extends Shape{
 	
 	public double orientation[] = new double[3];
