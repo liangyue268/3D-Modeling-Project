96986ab7a5962b4690533a25ba2fdfc6183a7334
cscoyle <cscoyle4@gmail.com>
2015-12-17 15:45:36 -0500

    added comments

--- Final Project Java/src/mod3D/Sphere.java
+++ Final Project Java/src/mod3D/Sphere.java
@@ -1,22 +1,26 @@
+//Author: Raya Rahman
 package mod3D;
 import java.awt.Color;
 
+//Represents a sphere shape, is a child of class Shape3D
 public class Sphere extends Shape3D {
 
 public double radius;
 
+//Defines a sphere based on all parameters of a Shape3D along with a radius.
 public Sphere(double r, double orientation[], Color c, int center[]){
 	super(center, c, orientation);
 	radius = r;
 
 
 }
+//Returns volume of a sphere
 double getVolume(){
 	return (4/3)*Math.PI*radius*radius*radius;
 
 }
 
-
+//Returns surface area of a sphere
 double getArea(){
 	return 4*Math.PI*radius*radius;
 }
