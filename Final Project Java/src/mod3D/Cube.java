e4b9010b681bd23a11096743093c8283437ed6fa
pvishwak <pvishwak@stevens.edu>
2015-12-18 12:25:38 -0500

    .

--- Final Project Java/src/mod3D/Cube.java
+++ Final Project Java/src/mod3D/Cube.java
@@ -1,32 +1,31 @@
 //Pratibha Vishwakarma
 
+
+
 package mod3D;
 import java.awt.Color;
+
 
 //class Cube Represents a cube shape, is a child of class Shape3D
 public class Cube extends Shape3D{
 
-	public double length, width, height;
-
+	public float side;
 	//Defines a cube by all parameters of a Shape3D along with length, width and height.
-	public Cube(double l, double w, double h, Color c, double orientation[], int center[] ){
+	public Cube(float side, Color c, double orientation[], int center[] ){
 		
 		super(center, c, orientation);
-		
-		length = l;
-		width = w;
-		height = h;
+		this.side = side;
 		
 	}
 	
 	//Returns the volume of the cube
-	double getVolume() {
-		return length * width * height;
+	float getVolume() {
+		return this.side  * this.side  * this.side;
 	}
 	
 	//Returns the area of the cube
-	double getArea() {
-		return 2 * (length * width + length * height + width * height);
+	float getArea() {
+		return 2 * (this.side  * this.side + this.side  * this.side  + this.side  * this.side);
 	}
-	
 }
+
